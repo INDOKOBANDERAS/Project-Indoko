0c695229c4c8f69ce0e95f3f3ec7f7d9d57a4af8
INDOKOBANDERAS <241230049@unmuhpnk.ac.id>
2026-04-29 12:36:04 +0700

    update file

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,11 +7,11 @@
     <title>@yield('title')</title>
 </head>
 
-<body>
-    class="min-h-screen flex flex-col bg-gray-100">
+<body class="min-h-screen flex flex-col bg-gray-100">
     @include('partials.header')
     <main class="flex-grow container mx-auto p-6">
         @yield('content')
-</main>
+    </main>
+    @include('partials.footer')
 </body>
 </html>
